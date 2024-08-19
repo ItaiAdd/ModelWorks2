5c30e716e3efb07077f9406daeb1a7361b9d42f0
ItaiAdd <itaiadd99@gmail.com>
2024-08-19 14:17:32 +0100

    implemented and documented FloatDist

--- modelworks2/distributions.py
+++ modelworks2/distributions.py
@@ -1,14 +1,19 @@
 import numpy as np
 import abc
 from typing import List, Any
+import warnings
 
 
 class BaseDistribution(abc.ABC):
+    """
+    Abstract base class for all parameter distributions. This defines
+    the methods a standard or custom parameter distribution must implement. 
+    """
 
     @abc.abstractmethod
     def sample(self, n:int=1) -> Any|List[Any]:
         """
-        draws n samples from the parameter space
+        Draws n samples from the parameter space
         and returns them as a list.
 
         Parameters
@@ -29,7 +34,7 @@ class BaseDistribution(abc.ABC):
     @abc.abstractmethod
     def sample_unique(self, n:int) -> List[Any]:
         """
-        draws n unique samples from the parameter space
+        Draws n unique samples from the parameter space
         and returns them as a list.
 
         Parameters
@@ -44,3 +49,124 @@ class BaseDistribution(abc.ABC):
         """
 
         raise NotImplementedError
+    
+
+
+class FloatDist(BaseDistribution):
+    """
+    Distribution class for parameters of type float.
+
+    Attributes
+    ----------
+    min_val: float
+        Minimum allowed value of the parameter.
+
+    max_val: float
+        Maximum allowed value of the parameter.
+
+    step: float (default is None)
+        Minumum difference between different sample values.
+
+    log: bool (default is False)
+        If True, samples are drawn from a log-uniform distribution, if False,
+        samples are drawn from a regular uniform distribution.
+
+        
+    Methods
+    -------
+    sample
+        Returns a requested number of samples from a uniform or log-uniform
+        distribution between min_val and max_val. If step is specified, all
+        non-identical sampled values are separated by at least step.
+
+    sample_unique
+        Returns unique sampled values from a uniform or log-uniform distribution
+        between min_val and max_val. If step is specified, all sampled values are
+        separated by at least step.
+        
+    """
+
+    def __init__(self, min_val:float, max_val:float,
+                    step:float|None=None, log:bool=False) -> None:
+        
+        self.min_val = min_val
+        self.max_val = max_val
+        self.step = step
+        self.log = log
+
+        # If a step is specified, find the maximum number of unique values 
+        # between min_val and max_val separated by step. 
+        if step:
+            self.max_unique_sample_size = ((max_val - min_val)//step)*step + 1
+        
+
+
+    def sample(self, n:int) -> List[float]:
+        """
+        Samples a uniform or log-uniform distribution between self.min_val and self.max_val.
+        If self.log is True, values are sampled from a log-uniform distribution and from a
+        regular uniform distribution otherwise. If self.step is True, all sampled values are
+        rounded to the nearest multiple of self.step greater than or equal to self.min_val. 
+
+        Parameters
+        ----------
+        n: int
+            number of samples to draw.
+
+        Returns
+        -------
+        sample: list of floats
+            The sampled parameter values. If self.log is True, values are sampled from
+            a log-uniform distribution and from a regular uniform distribution otherwise.
+            If self.step is True, all sampled values are rounded to the nearest multiple
+            of self.step greater than or equal to self.min_val. 
+        """
+        if self.log:
+            sample = np.random.uniform(np.log(self.min_val), np.log(self.max_val), n)
+        else:
+            sample = np.random.uniform(self.min_val, self.max_val, n)
+
+        if self.step:
+            sample = ((sample - self.min_val) // self.step) * self.step + self.min_val
+        
+        return list(sample)
+    
+
+    def sample_unique(self, n:int) -> List[float]:
+        """
+        Attempts to find n unique samples of the parameter by repeatedly calling self.sample 
+        (max attempts = 1000). If self.step does not allow n unique values between self.min_val
+        and self.max_val, the method attemps to find the maximum possible unique samples,
+        self.max_unique_sample_size. If self.max_unique_sample_size have not been found,
+        whatever has been found is returned with a warning message.
+
+        Parameters
+        ----------
+        n: int
+            Number of values to find.
+        
+        Returns
+        -------
+        sample: list[floats]
+            len(sample) <= self.max_unique_sample_size.
+        """
+        if self.step and (self.max_unique_sample_size < n):
+            n_allowed = int(self.max_unique_sample_size)
+            warnings.warn(f"{n} unique samples are impossible with step={self.step}. "
+                          f"{self.max_unique_sample_size} is the maximum possible number of unique samples.")
+        else:
+            n_allowed = n
+
+        sample = set()
+        trys = 0
+
+        while (len(sample) < n_allowed) and trys<1000:
+            s = self.sample(n_allowed)
+            sample.update(s)
+            trys += 1
+        
+        if (trys == 1000) and (len(sample) < n_allowed):
+            warnings.warn(f"Failed to find maximum possible unique samples. "
+                          f"Maximum is {n_allowed} but only found {len(sample)}")
+            
+        return list(sample)
